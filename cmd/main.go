2768dab6e51153298f3c40735f4e306c4d11db2a
Сотниченко Алексей <haliivi@mail.ru>
2025-10-29 14:49:45 +0700

    + base structure http server

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/haliivi/go-todo-app"
+	"github.com/haliivi/go-todo-app/pkg/handler"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+	srv := new(todo.Server)
+	if err := srv.Run("8888", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
